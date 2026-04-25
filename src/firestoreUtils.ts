ce2ad4dc65be35545ad453856f78ab0e1389971a
debbiebli262-byte <debbiebli262@gmail.com>
2026-04-25 21:54:13 +0300

    Add files via upload

--- src/firestoreUtils.ts
+++ src/firestoreUtils.ts
@@ -31,7 +31,7 @@ export function handleFirestoreError(error: unknown, operationType: OperationTyp
     path
   };
   console.error('Firestore Error: ', JSON.stringify(errInfo));
-  throw new Error(JSON.stringify(errInfo));
+  return errInfo;
 }
 
 export async function getArtistByName(name: string) {
